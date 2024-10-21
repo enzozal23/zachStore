35747d3cf98f3a91548f0a3d20ced57312d96662
enzozal23 <enzo.z.1.ez@gmail.com>
2024-10-21 14:37:14 -0300

    api

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8080/'
+    baseURL: 'https://zachpage.onrender.com/'
     ,
     withCredentials: true
 })
